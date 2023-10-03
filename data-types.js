6e82eec70ad8c0d3a65820326016e61e587134e9
Eddie Zaboroskie <ezaboroskie@MacBook-Pro-2.ftsbninc.local>
2023-10-03 14:20:26 -0400

    initializes js intro repo

---
+++ data-types.js
@@ -0,0 +1,16 @@
+// This is a simple introduction to JavaScript basics
+
+// Data Types: String, Number and Bigint, Boolean, Object, Symbol, Undefined, and Null
+// Strings: A string is read as text and is enclosed in quotes
+
+let string1 = 'Hello'
+let string2 = "World"
+
+
+// strings can contain numbers but will only be read and evaluated as a string as long as the quotes encompass the number
+let stringNumber = '1'
+
+//concatString will concatinate into the string '12'
+let concatString = '1' + '2'
+
+
